7b63096e3204f5000791666b4e092ea47ae9de87
Haskin <haskinsgu@aslcn.com.cn>
2021-12-28 18:12:11 +0800

    1232333

--- test/src/App.js
+++ test/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Re133333888
+          Learn 
         </a>
       </header>
     </div>
